46f185d85c25a4b36c80c2acc6578b4cf8ff997a
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2017-05-11 11:43:15 +0100

    Added travis and updated versions

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
     name="sPyNNaker7NewModelTemplate",
-    version="1.0.0",
+    version="1!4.0.0a5",
     packages=['python_models7',],
     package_data={'python_models7.model_binaries': ['*.aplx']},
-    install_requires=['SpyNNaker >= 3.0.0, < 4.0.0']
+    install_requires=['SpyNNaker >= 1!4.0.0a5, < 1!4.0.0']
 )
