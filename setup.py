1d4aea546260bbc9fff04b8b52f268cbdde5bb19
Christian <brenninc@cs.man.ac.uk>
2017-06-12 09:18:22 +0100

    pep8

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="sPyNNaker7NewModelTemplate",
     version="1!4.0.0a5",
-    packages=['python_models7',],
+    packages=['python_models7'],
     package_data={'python_models7.model_binaries': ['*.aplx']},
     install_requires=['SpyNNaker >= 1!4.0.0a5, < 1!5.0.0']
 )
