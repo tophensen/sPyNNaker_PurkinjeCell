b8fe65b64226353db3d8d02aa0ebb241a9bb3d05
Christian Brenninkmeijer <brenninc@cs.man.ac.uk>
2017-09-18 14:32:06 +0100

    list all spinnaker dependencies

--- setup.py
+++ setup.py
@@ -5,5 +5,15 @@ setup(
     version="1!4.0.0a5",
     packages=['python_models7'],
     package_data={'python_models7.model_binaries': ['*.aplx']},
-    install_requires=['SpyNNaker >= 1!4.0.0a5, < 1!5.0.0']
+    install_requires=[
+        'SpiNNUtilities >= 1!4.0.0a5, < 1!5.0.0',
+        'SpiNNStorageHandlers >= 1!4.0.0a5, < 1!5.0.0',
+        'SpiNNMachine >= 1!4.0.0a5, < 1!5.0.0',
+        'SpiNNMan >= 1!4.0.0a5, < 1!5.0.0',
+        'SpiNNaker_PACMAN >= 1!4.0.0a5, < 1!5.0.0',
+        'SpiNNaker_DataSpecification >= 1!4.0.0a5, < 1!5.0.0',
+        'spalloc >= 0.2.2, < 1.0.0',
+        'SpiNNFrontEndCommon >= 1!4.0.0a5, < 1!5.0.0',
+        'sPyNNaker >= 1!4.0.0a5, < 1!5.0.0',
+        'SpyNNaker7 >= 1!4.0.0a5, < 1!5.0.0']
 )
